46e63da2ffaa78d85f74375fb938a104f276d524
JohnPark0 <pji9663@gmail.com>
2021-10-31 23:10:05 +0900

    linked list done

--- working1.c
+++ working1.c
@@ -5,13 +5,50 @@
 #include <sys/types.h>
 #include <signal.h>
 
+
+
+typedef struct node {
+	struct node* next;
+	int data;
+} node;
+
+typedef struct list {
+	node* head;
+	node* tail;
+	int list_num;
+
+} list;
+
+void addnode(list* list, int data);
+int delnode(list* list);
+
 int pids[10];
 
 int main(int argc, char* arg[]) {
-
 	int i;
 	int ret;
-	for (i = 0; i < 10; i++) {
+	list* list = malloc(sizeof(list));
+	list->head = NULL;
+	list->tail = NULL;
+	list->list_num = 0;
+
+	addnode(list, 1);
+	addnode(list, 2);
+	addnode(list, 3);
+	addnode(list, 4);
+	addnode(list, 5);
+	i = delnode(list);
+	printf("%d\n",i);
+	i = delnode(list);
+	printf("%d\n", i);
+	i = delnode(list);
+	printf("%d\n", i);
+	i = delnode(list);
+	printf("%d\n", i);
+	i = delnode(list);
+	printf("%d\n", i);
+
+	/*for (i = 0; i < 10; i++) {
 		ret = fork();
 		if (ret > 0) {
 			pids[i] = ret;
@@ -25,8 +62,46 @@ int main(int argc, char* arg[]) {
 
 	for (i = 0; i < 10; i++) {
 		kill(pids[i], SIGKILL);
-	}
-
-
+	}*/
 	return 0;
+}
+
+void addnode(list* list, int data) {
+	node* addnode = (node*)malloc(sizeof(node));
+	addnode->next = NULL;
+	addnode->data = data;
+	if (list->head == NULL) {				//if add fist node
+		list->head = addnode;
+		list->tail = addnode;
+		printf("Add first node\n");
+	}
+	else {
+		list->tail->next = addnode;
+		list->tail = addnode;
+		printf("Add node\n");
+	}
+}
+
+int delnode(list* list) {
+	int data;
+	node* delnode;
+
+	if (list->head == NULL) {
+		printf("There is no node to delete\n");
+		return 0;
+	}
+	delnode = list->head;
+	data = list->head->data;
+	if (list->head->next == NULL) {		//if del last node
+		list->head = NULL;
+		list->tail = NULL;
+		printf("Delete last node\n");
+	}
+	else {
+		list->head = delnode->next;
+		printf("Delete node\n");
+	}
+	free(delnode);
+
+	return data;
 }
