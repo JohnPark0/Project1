46e453cf4d096d6666f1820d32a54e8cdc03b8c2
JohnPark0 <pji9663@LAPTOP-92212LBJ.localdomain>
2021-11-04 00:38:53 +0900

    done : test cpu_bust on parents proc
    to do :
    test cpu_bust on child proc <signal_handler send Signal to child proc
    test>

--- working1.c
+++ working1.c
@@ -26,27 +26,43 @@ void addnode(list* list, int data);
 //int delnode(list* list);
 int delnode(list* list, node* return_node);
 void inilist(list* list);
-
 void signal_handler(int signo);
-
+int child_proc_num = 0;		//편의를 위해 자식 프로세스 번호지정
+//child_proc_num 을 통해 각 자식 프로세스가 bust_time[child_proc_num]으로 남은 버스트 타임 계산
+int bust_time[10];
+node* RunningQ;
 int pids[10];
+
+
 
 int main(int argc, char* arg[]) {
 	int i;
 	int ret;
-	node* Return_node = malloc(sizeof(node));
-	list* RunningQ = malloc(sizeof(list));
-	inilist(RunningQ);
-	list* list = malloc(sizeof(list));
-	inilist(list);
 	
-	/*list->head = NULL;
-	list->tail = NULL;
-	list->list_num = 0;*/
+	node* Return_node = malloc(sizeof(node));
+	list* ReadyQ = malloc(sizeof(list));
+	inilist(ReadyQ);
 
-	int bust_time[MAX_PROC];
-	int child_proc_num = 1;		//편의를 위해 자식 프로세스 번호지정
-	//child_proc_num 을 통해 각 자식 프로세스가 bust_time[child_proc_num]으로 남은 버스트 타임 계산
+	RunningQ = malloc(sizeof(node));
+
+	/*list* list = malloc(sizeof(list));
+	inilist(list);*/
+
+	//int bust_time[MAX_PROC];
+
+	struct sigaction old_sa;
+	struct sigaction new_sa;
+	memset(&new_sa, 0, sizeof(new_sa));
+	new_sa.sa_handler = &signal_handler;
+	sigaction(SIGALRM, &new_sa, &old_sa);
+
+	struct itimerval new_itimer, old_itimer;
+	new_itimer.it_interval.tv_sec = 1;
+	new_itimer.it_interval.tv_usec = 0;
+	new_itimer.it_value.tv_sec = 1;
+	new_itimer.it_value.tv_usec = 0;
+	
+	
 
 	//CPU bust 타임 임의 세팅
 	//추가 할 것 : setting.txt 파일등으로 미리 세팅된 파일을 불러와서 저장 or 세팅파일을 프로그램 시작시 지정하지 않으면 랜덤으로 생성(고려)
@@ -61,26 +77,31 @@ int main(int argc, char* arg[]) {
 	bust_time[8] = 100;
 	bust_time[9] = 600;
 
-	
+
 	//초기 자식 프로세스 생성 구간 - 부모 프로세스(처음 1번만 실행)
 	for (i = 0; i < 10; i++) {
 		sleep(1);
 		ret = fork();
+		child_proc_num++;
 		if (ret > 0) {								//부모 프로세스
 			pids[i] = ret;
-			printf("pid[%d] : stop\n",pids[i]);
-			//kill(pids[i], SIGSTOP);
-			addnode(RunningQ, i);
+			printf("pid[%d] : stop\n", pids[i]);
+			addnode(ReadyQ, i);
 
 		}
-	//초기 자식 프로세스 생성 구간
+		//초기 자식 프로세스 생성 구간
 
-		//자식 프로세스 코드 구간 - 부모 프로세스가 kill 시그널 혹은 일정 자식 프로세스의 일정 조건까지 반복 후 종료
+			//자식 프로세스 코드 구간 - 부모 프로세스가 kill 시그널 혹은 일정 자식 프로세스의 일정 조건까지 반복 후 종료
 		else if (ret == 0) {						//자식 프로세스
-			kill(getpid(), SIGSTOP);				//첫 실행시 자식 프로세스 스스로 정지 시그널
+			//kill(getpid(), SIGSTOP);				//첫 실행시 자식 프로세스 스스로 정지 시그널
+			raise(SIGSTOP);
+
 			while (1) {								//루프가 없으면 한번 실행 후 자식 프로세스가 다른 자식 프로세스 무한 생성
 				printf("pid[%d] : work\n", getpid());
-				kill(getpid(), SIGSTOP);
+				kill(getpid(), SIGSTOP);			//임시코드
+
+
+
 			}
 		}
 		//자식 프로세스 코드 구간
@@ -90,13 +111,11 @@ int main(int argc, char* arg[]) {
 
 	//추가할 것 : 자식 프로세스의 CPU bust가 0이 될때까지 스케줄링
 	for (i = 0; i < 10; i++) {
+		
+		delnode(ReadyQ, RunningQ);				//Queue를 POP할때 Return_node에 해당 노드 저장
+		setitimer(ITIMER_REAL, &new_itimer, &old_itimer);
 		sleep(1);
-		delnode(RunningQ, Return_node);
-
-
-
-		//printf("pid[%d] : start\n", pids[i]);
-		kill(pids[Return_node->data], SIGCONT);
+		kill(pids[RunningQ->data], SIGCONT);
 	}
 
 
@@ -107,9 +126,22 @@ int main(int argc, char* arg[]) {
 	}
 
 	//부모 프로세스 코드 구간
-	
+
 	return 0;
 }
+
+//부모 프로세스에서 cpu_bust 감소
+//signal_handler에서 다시 자식 프로세스로 시그널 보내서 자식 프로세스에서 감소 테스트 해봐야
+void signal_handler(int signo)
+{
+	int child_proc;
+	int temp;
+	child_proc = pids[RunningQ->data];
+	temp = bust_time[RunningQ->data];
+	bust_time[RunningQ->data] = bust_time[RunningQ->data] - 100;
+	printf("pids[%d] bust_time - 100 (%d - 100 = %d)\n",child_proc,temp, bust_time[RunningQ->data]);
+}
+
 
 //헤더로 분리할 함수들 - 1	(가명 list.h)
 
@@ -118,8 +150,6 @@ void inilist(list* list) {
 	list->tail = NULL;
 	list->list_num = 0;
 }
-
-
 
 void addnode(list* list, int data) {
 	node* addnode = (node*)malloc(sizeof(node));
