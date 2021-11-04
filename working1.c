8602e09a4d57be2ab2260a9656e309b93334aac0
JohnPark0 <pji9663@LAPTOP-92212LBJ.localdomain>
2021-11-05 00:02:09 +0900

    done : parents_proc -> child_proc -> parents_proc signal send test done if child_proc know parents_pid can send signal to parents_proc

--- working1.c
+++ working1.c
@@ -22,8 +22,9 @@ typedef struct list {
 
 } list;
 
-void signal_decrease(int signo);
+void signal_bustend(int signo);
 
+void signal_decrease(int signo);
 void addnode(list* list, int data);
 //int delnode(list* list);
 int delnode(list* list, node* return_node);
@@ -31,10 +32,13 @@ void inilist(list* list);
 void signal_handler(int signo);
 int child_proc_num = 0;		//편의를 위해 자식 프로세스 번호지정
 //child_proc_num 을 통해 각 자식 프로세스가 bust_time[child_proc_num]으로 남은 버스트 타임 계산
-int bust_time[10];
-node* RunningQ;
-int pids[10];
 
+int bust_time[MAX_PROC];
+int pids[MAX_PROC];
+int parents_pid;
+
+list* WaitingQ;
+node* RunningQ;
 
 int main(int argc, char* arg[]) {
 	int i;
@@ -43,6 +47,9 @@ int main(int argc, char* arg[]) {
 	node* Return_node = malloc(sizeof(node));
 	list* ReadyQ = malloc(sizeof(list));
 	inilist(ReadyQ);
+
+	WaitingQ = malloc(sizeof(list));
+	inilist(WaitingQ);
 
 	RunningQ = malloc(sizeof(node));
 
@@ -68,6 +75,11 @@ int main(int argc, char* arg[]) {
 	act.sa_handler = &signal_decrease;
 	sigaction(SIGUSR1, &act, NULL);
 
+	struct sigaction act2;
+	memset(&act, 0, sizeof(act2));
+	act2.sa_handler = &signal_bustend;
+	sigaction(SIGUSR2, &act2, NULL);
+
 	//CPU bust 타임 임의 세팅
 	//추가 할 것 : setting.txt 파일등으로 미리 세팅된 파일을 불러와서 저장 or 세팅파일을 프로그램 시작시 지정하지 않으면 랜덤으로 생성(고려)
 	bust_time[0] = 100;
@@ -76,21 +88,20 @@ int main(int argc, char* arg[]) {
 	bust_time[3] = 200;
 	bust_time[4] = 100;
 	bust_time[5] = 300;
-	bust_time[6] = 100;
+	bust_time[6] = 0;
 	bust_time[7] = 400;
 	bust_time[8] = 100;
 	bust_time[9] = 600;
 
+	parents_pid = getpid();
 
 	//초기 자식 프로세스 생성 구간 - 부모 프로세스(처음 1번만 실행)
 	for (i = 0; i < 10; i++) {
 		sleep(1);
 		ret = fork();
-		
 		if (ret > 0) {								//부모 프로세스
 			child_proc_num++;
 			pids[i] = ret;
-			printf("pid[%d] : stop\n", pids[i]);
 			addnode(ReadyQ, i);
 
 		}
@@ -98,14 +109,13 @@ int main(int argc, char* arg[]) {
 
 			//자식 프로세스 코드 구간 - 부모 프로세스가 kill 시그널 혹은 일정 자식 프로세스의 일정 조건까지 반복 후 종료
 		else if (ret == 0) {						//자식 프로세스
-			//kill(getpid(), SIGSTOP);				//첫 실행시 자식 프로세스 스스로 정지 시그널
 			printf("pid[%d] = proc num [%d]\n",getpid(),child_proc_num);
+			printf("pid[%d] : stop\n", pids[i]);
 			raise(SIGSTOP);
-
+			
 			while (1) {								//루프가 없으면 한번 실행 후 자식 프로세스가 다른 자식 프로세스 무한 생성
 				printf("pid[%d] : work\n", getpid());
 				kill(getpid(), SIGSTOP);			//임시코드
-				
 
 
 			}
@@ -117,7 +127,7 @@ int main(int argc, char* arg[]) {
 
 	//추가할 것 : 자식 프로세스의 CPU bust가 0이 될때까지 스케줄링
 	for (i = 0; i < 10; i++) {
-		delnode(ReadyQ, RunningQ);				//Queue를 POP할때 Return_node에 해당 노드 저장
+		delnode(ReadyQ, RunningQ);				//Queue를 POP할때 RunningQ에 해당 노드 저장
 		setitimer(ITIMER_REAL, &new_itimer, &old_itimer);
 		sleep(10);
 		kill(pids[RunningQ->data], SIGCONT);
@@ -137,27 +147,30 @@ int main(int argc, char* arg[]) {
 
 //부모 프로세스에서 cpu_bust 감소
 //signal_handler에서 다시 자식 프로세스로 시그널 보내서 자식 프로세스에서 감소 테스트 해봐야
-void signal_handler(int signo)
+void signal_handler(int signo)				//부모 프로세스에서 작동
 {
 	int child_proc;
-	//int temp;
-	child_proc = pids[RunningQ->data];
-	//temp = bust_time[RunningQ->data];
-
-	//테스트용 부모 프로세스에서 cpu_bust감소--------------------------------------------------------------------
-	/*bust_time[RunningQ->data] = bust_time[RunningQ->data] - 100;
-	printf("pids[%d] bust_time - 100 (%d - 100 = %d)\n",child_proc,temp, bust_time[RunningQ->data]);*/
-	//테스트용---------------------------------------------------------------------------------------------------
+	child_proc = pids[RunningQ->data];		//Running Queue의 자식 프로세스 pid 지정
 
 	kill(child_proc, SIGUSR1);				//자식 프로세스에 SIGUSR1 시그널 전송 -> sigaction으로 signal_decrease함수 호출
 }
 
-void signal_decrease(int signo) {
+void signal_decrease(int signo) {			//실행중인 자식 프로세스에서 작동
 	int temp;
 	temp = bust_time[child_proc_num];
 
-	bust_time[child_proc_num] = bust_time[child_proc_num] - 100;
-	printf("pids[%d] = cpu_bust decrease %d - 100 = %d\n", getpid(), temp ,bust_time[child_proc_num]);
+	if (bust_time[child_proc_num] == 0) {
+		kill(parents_pid, SIGUSR2);
+	}
+	else {
+		bust_time[child_proc_num] = bust_time[child_proc_num] - 100;
+		printf("pids[%d] = cpu_bust decrease %d - 100 = %d\n", getpid(), temp, bust_time[child_proc_num]);
+	}
+}
+
+void signal_bustend(int signo) {			//부모 프로세스
+	addnode(WaitingQ, RunningQ->data);
+	printf("SIGUSR2 called\n");
 }
 
 
